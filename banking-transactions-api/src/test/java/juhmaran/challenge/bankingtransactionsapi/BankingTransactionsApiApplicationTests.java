96d7e0cb3327dcceefe7b3ccc70e5a728b97bedb
JulianeMaran32 <julianemaran@gmail.com>
2025-06-10 18:20:53 -0300

    feat: add new project

---
+++ banking-transactions-api/src/test/java/juhmaran/challenge/bankingtransactionsapi/BankingTransactionsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package juhmaran.challenge.bankingtransactionsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingTransactionsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
